7b93f91438a4a824df967bf8759b5373d5dd0252
mharikmert <mhilmiarikmert@gmail.com>
2021-03-17 22:10:33 +0300

    main component is created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import React from 'react'
+import Header from './components/Header';
+import Button from './components/Button';
+import Result from './components/Result'; 
+import Dice from './components/Dice';
+import PlayerPlace from './components/PlayerPlace';
+import Score from './components/Score';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  // should be returned a single element
+  const [count, setCount] = useState(0);
+  const [score,number, setScore,setNumber] = useState();
+    return  (
+      <div className = 'container'>
+          <Header/> 
+        <div className = 'btnContainer'>
+          <Button text = '&minus;' onClick = {() => setCount(count-1)}/>
+          <Result result = {count}/> 
+          <Button text = '&#43;' onClick = {() => setCount(count+1)}/> 
+        </div>
+          {/* <Dice/> */}
+          <div className = "player1-container" > 
+            <PlayerPlace number = {1} color = "green" score = {count} />
+          </div>
+          <div className = "player2-container"> 
+            <PlayerPlace number = {2} color = "green" score = {count} />
+          </div>
+      </div>
+      // <h2> that does not work </h2>
+    )
 }
 
+// class App extends React.Component{
+//   render(){
+//     return (
+//     <div className = 'container'>
+//         <h1> welcome to the pig game </h1>
+//         <Header/>
+
+//     </div>
+//     )
+// }
+// }
 export default App;
