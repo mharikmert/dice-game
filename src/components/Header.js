c6af2c48cab011e178b3718e7aff1e83ae1c826e
mharikmert <mhilmiarikmert@gmail.com>
2021-03-20 22:57:13 +0300

    header styling stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
 const Header = (props) => {
     return (
         <header>
-            <h1> WELCOME TO THE DICE GAME <br></br> </h1>
+            <h2 style = {headerStyle}> WELCOME TO THE DICE GAME <br></br> </h2>
         </header>
     )
 }
-export default Header;
-
 Header.defaultProps = {
     title: 'default title from props',
     something : 'this is also a prop'
 }
 
-// CSS in JS
-// const headerStyle = {
-//     color: 'red', backgroundColor: 'yellow'
-// }
+const headerStyle = {
+    margin: ' 30px auto',
+    fontFamily: 'sans serif',
+    fontWeight: 'bold',
+}
+export default Header;
+
