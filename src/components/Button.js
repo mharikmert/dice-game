c5904eb1e51395f022a9634d602d62dcbba88314
mharikmert <mhilmiarikmert@gmail.com>
2021-03-22 15:28:29 +0300

    outer div for buttons and disabled prop depends on the activity

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,9 @@
-const Button = ({text, onClick, buttonClicked}) => {
-    return <button className = 'btn' onClick = {onClick}> 
-            {buttonClicked && <span className = "spinner-border spinner-border-sm"></span>}
-            {text} 
-           </button> 
+const Button = ({text, onClick, buttonClicked, disabled}) => {
+    return <div> 
+                <button disabled = {disabled} className = {disabled ? 'btn btn-disabled': 'btn'} onClick = {onClick}> 
+                    {buttonClicked && <span className = "spinner-border spinner-border-sm"></span>}
+                    {text} 
+                </button>
+            </div>
 }
 export default Button
