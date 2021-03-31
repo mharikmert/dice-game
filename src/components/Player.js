a572b6ceab8730051fabe1eb8deac7ca937282a6
mharikmert <mhilmiarikmert@gmail.com>
2021-03-31 21:34:45 +0300

    additional functionalities in player js

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,9 +5,10 @@ const Player = props => {
         <div className = {props.isActive ? 'player-container': 'player-container toggle-player'} >
           <p> PLAYER {props.number}</p>
           <Score score = {props.score}/>
-          <Button disabled = {!props.isActive} text = 'Throw Dice'> </Button>
-          <Button disabled = {!props.isActive} text = 'Hold Score'></Button>
+          <Button disabled = {!props.isActive} text = 'Roll Dice' onClick = {props.roll} buttonClicked = {props.buttonClicked}></Button>
+          <Button disabled = {!props.isActive} text = 'Hold Score' onClick = {props.hold} buttonClicked = {props.buttonClicked}></Button>
         </div>
     )
 }
+
 export default Player
